32a58881d229da232f6b1157a99d4d9e50c17107
umutfurkancan <42351761+umutfurkancan@users.noreply.github.com>
2021-01-04 17:24:46 +0300

    1
    1

--- NativeRestaurantSearc-master/src/api/zomato.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-export default axios.create({
-  baseURL: 'https://developers.zomato.com/api/v2.1',
-  headers: {
-    'user-key': '264f4e322ed1f65385eec84294bfa924'
-  }
-});
